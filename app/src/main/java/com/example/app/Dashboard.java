adf6f4f371edd1f73105cfeac2d11218f1f049fc
Abdul Junaid <abdulmjunaid@gmail.com>
2022-11-10 18:42:38 -0500

    Deleted Button Implementation
    kept crashing app

--- app/src/main/java/com/example/app/Dashboard.java
+++ app/src/main/java/com/example/app/Dashboard.java
@@ -45,29 +45,6 @@ public class Dashboard extends AppCompatActivity{
 
     }
 
-    ActivityResultLauncher<Intent> sActivityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK){
-                        Intent data = result.getData();
-                        Uri uri = data.getData();
-                    }
-                }
-            }
-    );
-
-    public void openFileDialog(View view){
-        Intent data = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        data.addCategory(Intent.CATEGORY_OPENABLE);
-        data.setType("*/*");
-        String[] mimeTypes = {"text/*"};
-        data.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-        data = Intent.createChooser(data, "Choose a file");
-        sActivityResultLauncher.launch(data);
-    }
-
 
     private void setGoal() {
         Float spending = Float.valueOf(editspending.getText().toString());
