719da20a0f58562efb1a17541476f205938afda6
mushikhan1102 <mushikhan1102@gmail.com>
2022-11-11 20:06:55 -0500

    foreground push app
    forreground push app

---
+++ app/src/main/java/com/example/app/MyFirebaseMessagingService.java
@@ -0,0 +1,115 @@
+package com.example.app;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        System.out.println("From: " + remoteMessage.getFrom());
+
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            System.out.println("Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        sendNotification(remoteMessage.getFrom(), remoteMessage.getNotification().getBody());
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+
+    // implementation
+    private void sendNotification(String from, String body){
+
+        new Handler(Looper.getMainLooper()).post(new Runnable(){
+
+            @Override
+            public void run(){
+
+                Toast.makeText(MyFirebaseMessagingService.this.getApplicationContext(),
+                        from + " -> " + body,Toast.LENGTH_SHORT);
+            }
+
+        });
+
+    }
+
+    // copied source code defining SendNotification to display on notifications area
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_IMMUTABLE);
+
+        // can be set to any value and data type
+        String channelId = "My channel ID";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        // created an icon to display notification using identifier "notification
+                        .setSmallIcon(R.drawable.ic_stat_notification)
+                        // the title of the notification type
+                        .setContentTitle("Foreground notification")
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+
+    // worker class
+    public static class MyWorker extends Worker {
+
+        public MyWorker(@NonNull Context context,
+                        @NonNull WorkerParameters workerParams) {
+            super(context, workerParams);
+        }
+
+        @NonNull
+        @Override
+        public Result doWork() {
+            // TODO(developer): add long running task here.
+            return Result.success();
+        }
+
+    }
+
+}
