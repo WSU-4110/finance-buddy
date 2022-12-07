c08eaf52a830536e879fcade33fb0fd92a1b284a
bigdog765 <aidy.vg@gmail.com>
2022-12-06 23:31:13 -0500

    getting transactions

--- app/src/main/java/com/example/app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/app/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,18 +14,22 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.app.LoginActivity;
 import com.example.app.R;
-import com.example.app.RegisterUser;
-import com.example.app.User;
 import com.example.app.api.Client;
 import com.example.app.api.TokenHandler;
 import com.example.app.databinding.FragmentHomeBinding;
 import com.plaid.client.model.AccountBase;
 import com.plaid.client.model.AccountsGetRequest;
 import com.plaid.client.model.AccountsGetResponse;
+import com.plaid.client.model.Transaction;
+import com.plaid.client.model.TransactionsGetRequest;
+import com.plaid.client.model.TransactionsGetResponse;
 import com.plaid.client.request.PlaidApi;
 
 import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
 
 import retrofit2.Response;
 
@@ -33,11 +38,13 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
     private TextView accounts;
     private TextView balance;
-    private PlaidApi p = Client.getClient();
-    private Response<AccountsGetResponse> response = null;
-    private boolean c = false;
-    private String accounts_ui;
-    private String balance_ui;
+    private TextView titleT;
+    private Button checking, savings;
+    private PlaidApi client = Client.getClient();
+    private Response<AccountsGetResponse> responseAcc = null;
+    private Response<TransactionsGetResponse> responseTran = null;
+    private boolean c,d= false;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -53,27 +60,57 @@ public class HomeFragment extends Fragment {
 
         accounts = (TextView) root.findViewById(R.id.accounts_text);
         balance = (TextView) root.findViewById(R.id.accounts_balance);
+        checking = (Button) root.findViewById(R.id.b1);
+        savings = (Button) root.findViewById(R.id.b2);
+        titleT = (TextView) root.findViewById(R.id.title_Tran);
 
         if(LoginActivity.bankSetup){
             getAccountInfo();
         }
         else accounts.setText("No Accounts Setup");
+
+        checking.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                getCheckings();
+            }});
+        savings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                getSavings();
+            }});
         return root;
     }
-    public void getAccountInfo(){
-        AccountsGetRequest request = new AccountsGetRequest().accessToken(TokenHandler.ACCESS_TOKEN);
 
-        String s = request.toString();
-        Log.e("REQUESt for acc", s);
+    private void getSavings() {
+        titleT.setText("Savings Transactions:");
+    }
+
+    private void getCheckings(){
+        titleT.setText("Checkings Transactions:");
+
+        LocalDate startDate = null;
+        LocalDate endDate = null;
+
+        //Local date needs higher api version??
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            startDate = LocalDate.parse("2022-01-01");
+            endDate = LocalDate.parse("2022-12-31");
+        }
+
+        // Pull transactions for a date range
+        TransactionsGetRequest request = new TransactionsGetRequest().accessToken(TokenHandler.ACCESS_TOKEN)
+                .startDate(startDate)
+                .endDate(endDate);
+
         try{
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try{
-                        response = p.accountsGet(request).execute();
-                        c = true;
+                        responseTran = client.transactionsGet(request).execute();
+                        d = true;
                     } catch (Exception e) {
-                        //String fii = response.errorBody().toString();
                         e.printStackTrace();
                         Log.e("error message",e.getMessage());}}
             });
@@ -83,20 +120,48 @@ public class HomeFragment extends Fragment {
         }
 
         while(!c){};
-        List<AccountBase> accountsList = response.body().getAccounts();
+
+        //This is Plaid's Transaction class not ours
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        //fix 
+        transactions.addAll(responseTran.body().getTransactions());
+        for(int i = 0; i < transactions.size(); i++){
+            Log.e("t",transactions.get(i).getName());
+        }
+    }
+
+    public void getAccountInfo(){
+        AccountsGetRequest request = new AccountsGetRequest().accessToken(TokenHandler.ACCESS_TOKEN);
+        try{
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try{
+                        responseAcc = client.accountsGet(request).execute();
+                        c = true;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Log.e("error message",e.getMessage());}}
+            });
+            thread.start();
+        }catch (ExceptionInInitializerError e){
+            Log.e("tag response", e.getCause().toString());
+        }
+
+        while(!c){};
+        List<AccountBase> accountsList = responseAcc.body().getAccounts();
         String a = "";
         String b = "";
 
         DecimalFormat df = new DecimalFormat("0.00");
 
-
         for(int i = 0; i < accountsList.size(); i++){
+
             a = a.concat(accountsList.get(i).getName() + "\n");
             b = b.concat("$" + df.format(accountsList.get(i).getBalances().getCurrent())  + "\n");
         }
         accounts.setText(a);
         balance.setText(b);
-
     }
 
     @Override
